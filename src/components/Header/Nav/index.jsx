de75d1be2257af0bd1b4a25fe6f8e637b5ff16c7
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2023-09-26 17:54:37 -0300

    Criado o componente Nav
    Criado o componente Nav para reaproveitamento em outras páginas.

---
+++ src/components/Header/Nav/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './Nav.module.css'
+
+function Nav() {
+  return (
+    <nav>
+        <Link to={'/home'}>Início</Link>
+        <a href="#Back-end">Back-end</a>
+        <a href="#Mobile">Mobile</a>
+        <a href="#Front-end">Front-end</a>
+        <a href="#Game-Development">Game Development</a>
+        <a href="#Data-Base">Data Base</a>
+        <Link to={'/meusFavoritos'}>Meus Favoritos</Link>
+    </nav>
+  )
+}
+
+export default Nav
